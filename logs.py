889e7f0f51df914393a3599a52960612485a6ad0
kolysg <koly.sg@gmail.com>
2018-02-03 20:24:44 -0800

    first commit

---
+++ logs.py
@@ -0,0 +1,11 @@
+"""create a reporting tool
+	#prints out reports (in plain text)
+	#3 questions to answer
+		1. what are the most popular three articles -sorted list?
+			- how do i measure popularity? 
+		2. who are the most pop authors - sorted list
+		3. on which days did more than 1% of requests lead to errors?
+			- example: July 29, 2016 — 2.5% errors
+
+"""
+
