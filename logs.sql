0e27edbebe22fd0a741a890a525f1b982c3515d5
kolysg <koly.sg@gmail.com>
2018-02-07 11:59:00 -0800

    clean up views and logs

--- logs.sql
+++ logs.sql
@@ -11,9 +11,9 @@
 
 --question 1 - most popular three articles of all time
 
--- select articles_info.article_name, articles_info.click_count
--- 	from articles_info
--- 	limit 3;
+select articles_info.article_name, articles_info.click_count
+	from articles_info
+	limit 3;
 
 --question 2 - most popular authors
 -- select articles_info.author_name, articles_info.click_count
@@ -25,7 +25,8 @@
 -- 	on log.path like concat('%', author_info.article_name, '%')
 -- 	where log.status like '%200%'
 -- 	group by author_info.author_name
--- 	order by click_count desc;
+-- 	order by click_count desc
+-- 	limit 2;
 
 --question 3 - on which days did more than 1% of requests lead to error
 	--a table with time, and error status
@@ -35,29 +36,8 @@
 -- two views where one has the number of failed calls per day 
 -- and then the other with the number of total calls per day.
 
--- create a view that joins log table with article_info table
 
 
--- select cast(time as date),status, count(status) as count 
--- 	from log
--- 	where log.status like '%200%'
--- 	group by log.time, log.status
--- 	limit 2;
-
--- select a.time as day, a.total_req as total, b.error_req as error,
--- 	b.error_req::numeric * 100 / a.total_req::numeric as perc 
--- 		from total_requests_per_day as a join error_requests_per_day as b
--- 		on a.time = b.time
--- 		where (b.error_req::numeric * 100::numeric / a.total_req::numeric) > 1::numeric
--- 			limit 2;
-
--- create view high_error_days as 
-	select t.day as day,
-		round(e.total_errors::numeric * 100::numeric / t.total::numeric, 2) as percentage
-		from total_rqst as t join error_rqst as e
-			on t.day = e.day
-			where round(e.total_errors::numeric * 100::numeric / t.total::numeric, 2) > 1::numeric
-			limit 2;
 
 	
 
--- views.sql
+++ views.sql
@@ -12,47 +12,46 @@
 -- 		3. on which days did more than 1% of requests lead to errors?
 -- 			- example: July 29, 2016 — 2.5% errors
 
-
--- join authors with articles table to create a table that also has author's name
-
-CREATE VIEW author_info AS 
-	SELECT authors.name as author_name, articles.slug as article_name, articles.title as article_title, articles.author as author_id 
-	FROM articles, authors
-	WHERE authors.id = articles.author
-	ORDER BY authors.name;
+create view author_info as 
+	select authors.name as author_name, articles.slug as article_name, articles.title as article_title, articles.author as author_id 
+	from articles, authors
+	where authors.id = articles.author
+	order by authors.name;
 
 
 -- create a view with articles, how many success counts, author.
 create view articles_info as 
 	select a.article_name, a.author_name, count (*) as click_count 
-	from author_info as a inner join log
-	on log.path like concat('%', a.article_name, '%')
-	where log.status like '%200%'
-	group by a.article_name, a.author_name, log.path
-	order by click_count desc;
+		from author_info as a inner join log
+		on log.path like concat('%', a.article_name, '%')
+		where log.status like '%200%'
+		group by a.article_name, a.author_name, log.path
+		order by click_count desc;
+
+-- most three popular articles
+create view most_popular_articles as
+	select articles_info.article_name, articles_info.click_count
+		from articles_info
+		limit 3;
+
+-- most popular author
+create view most_popular_author as
+	select author_info.author_name, count(*) as click_count
+		from author_info inner join log
+		on log.path like concat('%', author_info.article_name, '%')
+		where log.status like '%200%'
+		group by author_info.author_name
+		order by click_count desc;
 
 
--- create view total_requests_per_day as
--- select cast(log.time as date), log.status, count(*) as total_req
--- 	from log
--- 	group by log.time, log.status
--- 	order by total_req desc
--- 	limit 2;
-
--- create view error_requests_per_day as 
--- 	select cast(log.time as date), log.status, count(*) as error_req
--- 		from log
--- 		where log.status like '%404%'
--- 		group by log.time, log.status
--- 		order by error_req desc
--- 		limit 2;
-
+-- total requests per day
 create view total_rqst as 
 	select date(log.time) as day, count(log.id) as total
 	from log
 	group by (date(log.time))
 	order by (date(log.time));
 
+-- total bad requests per day
 create view error_rqst as 
 	select date(log.time) as day, count(log.id) as total_errors
 	from log
@@ -60,5 +59,13 @@ create view error_rqst as
 	group by (date(log.time))
 	order by (date(log.time));
 
+
+-- days where bad requests are > 1%
+create view high_error_days as 
+	select t.day as day,
+		round(e.total_errors::numeric * 100::numeric / t.total::numeric, 2) as percentage
+		from total_rqst as t join error_rqst as e
+			on t.day = e.day
+			where round(e.total_errors::numeric * 100::numeric / t.total::numeric, 2) > 1::numeric;
 
 
